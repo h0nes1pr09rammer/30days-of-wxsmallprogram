5754c2256de6d4af7b1c2e42973fea6a77a7b415
lgg <15824444354@163.com>
2018-05-10 17:40:53 +0800

    修改

--- file_server.js
+++ file_server.js
@@ -19,19 +19,27 @@ var server = http.createServer(function (request, response) {
     var filepath = path.join(root, pathname);
     // 获取文件状态:
     fs.stat(filepath, function (err, stats) {
-        if (!err && stats.isFile()) {
-            // 没有出错并且文件存在:
-            console.log('200 ' + request.url);
+        if (!err && stats.isDirectory()){
+            console.log("stats.isDirectory()")
             // 发送200响应:
             response.writeHead(200);
             // 将文件流导向response:
-            fs.createReadStream(filepath).pipe(response);
-        } else {
-            // 出错了或者文件不存在:
-            console.log('404 ' + request.url);
-            // 发送404响应:
-            response.writeHead(404);
-            response.end('404 Not Found');
+            fs.createReadStream("index.html").pipe(response);
+        }else{
+            if (!err && stats.isFile()) {
+                // 没有出错并且文件存在:
+                console.log('200 ' + request.url);
+                // 发送200响应:
+                response.writeHead(200);
+                // 将文件流导向response:
+                fs.createReadStream(filepath).pipe(response);
+            } else {
+                // 出错了或者文件不存在:
+                console.log('404 ' + request.url);
+                // 发送404响应:
+                response.writeHead(404);
+                response.end('404 Not Found');
+            }
         }
     });
 });
